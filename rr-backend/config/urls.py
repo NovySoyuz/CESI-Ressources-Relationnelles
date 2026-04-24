4c34e7207c1e8320b5510f6e1b5ea5864d70b1f0
Adelin Casez <71118812+XFactors8@users.noreply.github.com>
2026-04-24 09:24:48 +0200

    Update urls.py
    remove of comments '#' on path

--- rr-backend/config/urls.py
+++ rr-backend/config/urls.py
@@ -8,8 +8,8 @@ def health(request):
 urlpatterns = [
     path('', health),
     path('api/health/', health),
-    #path('api/auth/', include('users.urls')),
-    #path('api/interactions/', include('interactions.urls')),
-    #path('api/resources/', include('resources.urls')),
+    path('api/auth/', include('users.urls')),
+    path('api/interactions/', include('interactions.urls')),
+    path('api/resources/', include('resources.urls')),
     path('api/administration/', include('administration.urls')),
 ]
